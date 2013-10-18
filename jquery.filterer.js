acf64aaf61eeb85488adfe1bb81d6a6046ac216d
Phillip Shum <phillipshum@hogarthww.com>
2013-10-18 15:32:28 +0100

    update code for zonza

--- jquery.filterer.js
+++ jquery.filterer.js
@@ -4,169 +4,209 @@ define(function(require) {
 
     $.fn.filterer = function(options) {
 
-        var mock = ['alpha', 'bravo', 'charlie', 'delta', 'echo'];
+        var mock = [
 
-        var filterer = {
+            {'label': 'label-a', 'value': 1, 'location': 'location-a'},
+            {'label': 'label-b', 'value': 2, 'location': 'location-b'},
+            {'label': 'label-c', 'value': 3, 'location': 'location-c'},
+            {'label': 'label-d', 'value': 4, 'location': 'location-d'},
+            {'label': 'label-e', 'value': 5, 'location': 'location-e'},
+            {'label': 'label-f', 'value': 6, 'location': 'location-f'},
+            {'label': 'label-g', 'value': 7, 'location': 'location-g'},
+            {'label': 'label-h', 'value': 8, 'location': 'location-h'},
+            {'label': 'label-i', 'value': 9, 'location': 'location-i'},
+            {'label': 'label-j', 'value': 10, 'location': 'location-j'}
+        ];
 
-            defaults: { },
-            init: function($el, options) {
+        var filterer = (function(options) {
 
-                this.$el = $el;
+            return {
 
-                this.settings = $.extend(this.defaults, options);
+                defaults: {
 
-                this.hideOriginalElement();
+                    getUrl: '',
+                    saveUrl: ''
+                },
+                init: function($el) {
 
-                this.createElements();
+                    this.$el = $el;
 
-                this.attachElements();
+                    this.settings = $.extend(this.defaults, options);
 
-                this.bindEvents();
+                    this.hideOriginalElement();
 
-                return this.$el;
-            },
-            hideOriginalElement: function() {
+                    this.controls = this.assembleControls(this.getControls());
 
-                this.$el.hide();
-            },
-            attachElements: function() {
+                    this.mirrorAttributes();
 
-                this.controls.$container.append(this.controls.$input);
+                    this.bindEvents();
 
-                this.controls.$container.append(this.controls.$resultsContainer);
+                    return this;
+                },
+                hideOriginalElement: function() {
 
-                this.controls.$resultsContainer.append(this.controls.$results);
+                    this.$el.hide();
+                },
+                assembleControls: function(controls) {
 
-                this.controls.$container.append(this.controls.$clear);
+                    controls.$container.append(controls.$input);
 
-                this.$el.after(this.controls.$container);
-            },
-            createElements: function() {
+                    controls.$container.append(controls.$resultsContainer);
 
-                this.controls = {};
+                    controls.$resultsContainer.append(controls.$results);
 
-                this.controls.$container = $('<section class="filterer"></section>');
+                    this.$el.after(controls.$container);
 
-                this.controls.$input = $('<input />');
+                    return controls;
+                },
+                getControls: function() {
 
-                this.controls.$resultsContainer = $('<div></div>');
+                    var controls = {};
 
-                this.controls.$results = $('<ul></ul>');
+                    controls.$container = $('<section class="filterer"></section>');
 
-                this.controls.$clear = $('<a></a>');
-            },
-            bindEvents: function() {
+                    controls.$input = $('<input />');
 
-                var $container = this.controls.$container;
+                    controls.$resultsContainer = $('<div></div>');
 
-                $container.on('click', function(e) { e.stopPropagation(); });
+                    controls.$results = $('<ul></ul>');
 
-                $container.on('keyup', this.controls.$input, this.onInputKeyUp.bind(this));
+                    return controls;
+                },
+                mirrorAttributes: function() {
 
-                $container.on('focusin', this.controls.$input, this.onInputFocusIn.bind(this));
-            
-                $container.on('click', 'li', this.onResultClick.bind(this));
-            },
-            onResultClick: function(e) {
+                    this.controls.$input.attr('placeholder', this.$el.attr('placeholder'))
+                },
+                bindEvents: function() {
 
-                e.stopImmediatePropagation();
+                    var $container = this.controls.$container;
 
-                console.log('onResultClick()');
+                    $container.on('click', function(e) { e.stopPropagation(); });
 
-                var $currentTarget = $(e.currentTarget);
+                    $container.on('keyup', this.controls.$input, this.onInputKeyUp.bind(this));
 
-                console.log($currentTarget.text());
+                    $container.on('focusin', this.controls.$input, this.onInputFocusIn.bind(this));
 
-                if (this.settings.onResultSelect) {
+                    $container.on('click', 'li', this.onResultClick.bind(this));
+                },
+                reset: function() {
 
-                    this.settings.onResultSelect();
+                    this.controls.$input.val('');
+
+                    this.controls.$results.empty();
+                },
+                onResultClick: function(e) {
+
+                    e.stopImmediatePropagation();
+
+                    console.log('onResultClick()');
+
+                    var $currentTarget = $(e.currentTarget);
+
+                    console.log($currentTarget.text());
+
+                    if (this.settings.onResultSelect) {
+
+                        this.settings.onResultSelect($currentTarget.data('value'));
+                    }
+
+                    this.hideList();
+
+                    this.reset();
+                },
+                onInputFocusIn: function(e) {
+
+                    console.log('onInputFocus()');
+
+                    var query = $(e.target).val();
+
+                    console.log(query);
+
+                    if (query.length > 0) {
+
+                        this.performSearch({ query: query });
+                    }
+                },
+                onInputKeyUp: function(e) {
+
+                    console.log('onKeyUp()');
+
+                    var query = this.controls.$input.val();
+
+                    this.performSearch({ query: query });
+                },
+                performSearch: function(query) {
+
+                    console.log('performSearch()');
+
+                    if (this.settings.applySearchParameters) {
+                        query = $.extend(query, this.settings.applySearchParameters());
+                    }
+
+                    console.log(query);
+
+                    $.when(this.getResults(query))
+                    .then(this.populateList.bind(this))
+                    .then(this.displayList.bind(this));
+                },
+                getResults: function(query) {
+
+                    console.log('getResults()');
+
+                    console.log(query);
+
+                    var dfd = new $.Deferred();
+
+                    $.ajax(this.settings.getUrl, { query: query })
+                    .done(function(data) {
+                        dfd.resolve(mock);
+                    })
+                    .fail(function() {
+                        dfd.resolve(mock);
+                    });
+
+                    return dfd;
+                },
+                populateList: function(data) {
+
+                    console.log('populateList');
+
+                    console.log(data);
+
+                    var i = 0, $list = this.controls.$results;
+
+                    $list.empty();
+
+                    for (i=0,l=data.length;i<l;i++) {
+
+                        $list.append('<li data-value="' + data[i].value + '" data-location="' + data[i].location + '">' + data[i].label + '</li>');
+                    }
+                },
+                displayList: function() {
+
+                    console.log('displayList()');
+
+                    if (!this.controls.$resultsContainer.hasClass('active')) {
+
+                        this.controls.$resultsContainer.addClass('active');
+
+                        $('body').one('click', this.hideList.bind(this));
+                    }
+                },
+                hideList: function() {
+
+                    console.log('hideList()');
+
+                    if (this.controls.$resultsContainer.hasClass('active')) {
+
+                        this.controls.$resultsContainer.removeClass('active');
+                    }
                 }
+            };
 
-                this.hideList();
-            },
-            onInputFocusIn: function(e) {
+        })(options);
 
-                console.log('onInputFocus()');
-
-                var query = $(e.target).val();
-
-                console.log(query);
-
-                if (query.length > 0) {
-
-                    this.performSearch(query);
-                }
-            },
-            onInputKeyUp: function(e) {
-
-                console.log('onKeyUp()');
-
-                var query = this.controls.$input.val();
-
-                this.performSearch(query);
-            },
-            performSearch: function(query) {
-                
-                $.when(this.getResults(query))
-                .then(this.populateList.bind(this))
-                .then(this.displayList.bind(this));
-            },
-            getResults: function(query) {
-
-                console.log('getResults()');
-
-                console.log(query);
-
-                var dfd = new $.Deferred();
-
-                $.ajax('/temp/', { query: query })
-                .done(function(data) {
-                    dfd.resolve(data);
-                })
-                .fail(function() {
-                    dfd.resolve(mock);
-                });
-
-                return dfd;
-            },
-            populateList: function(data) {
-
-                console.log('populateList');
-
-                console.log(data);
-
-                var i = 0, $list = this.controls.$results;
-
-                $list.empty();
-
-                for (i=0,l=data.length;i<l;i++) {
-
-                    $list.append('<li>' + data[i] + '</li>');
-                }
-            },
-            displayList: function() {
-
-                console.log('displayList()');
-
-                if (!this.controls.$resultsContainer.hasClass('active')) {
-
-                    this.controls.$resultsContainer.addClass('active');
-
-                    $('body').one('click', this.hideList.bind(this));
-                }
-            },
-            hideList: function() {
-
-                console.log('hideList()');
-
-                if (this.controls.$resultsContainer.hasClass('active')) {
-
-                    this.controls.$resultsContainer.removeClass('active');
-                }
-            }
-        }
-
-        return filterer.init(this, options);
+        return filterer.init(this);
     };
 
 });
